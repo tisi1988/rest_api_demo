82b3c3b747b28adf19d6fb125fc141f78fd7eedb
Ruben Sanchez Castellano <tisi1988@gmail.com>
2018-08-26 02:10:00 +0200

    UPDATE: Documentation.

--- Rest_api_demo/doc/html/search/functions_9.js
+++ Rest_api_demo/doc/html/search/functions_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['_7ecustomexception',['~CustomException',['../class_custom_exception.html#ac4fb927e4197e228ad8d88554a342f02',1,'CustomException']]],
+  ['_7edbcontroller',['~DBController',['../class_d_b_controller.html#ad47f8168fff9ade3de73c52ab774120e',1,'DBController']]],
   ['_7erequestsmanager',['~RequestsManager',['../class_requests_manager.html#aa59ae6352fcfa39cbba1167195376fbd',1,'RequestsManager']]],
   ['_7erestdemoapp',['~RestDemoApp',['../class_rest_demo_app.html#a06228a2541491ef126f842bb1a21bd37',1,'RestDemoApp']]]
 ];
